874ce6021ed13d82f5028f9237f928302e85dfdd
sushil kumar <isushilchaudhary@gmail.com>
2023-07-04 11:20:14 +0530

    add:useState hooks introdues

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card'
 
 function ExpenseItem(props) {
- 
+  const [title,setTitle] =  useState(props.title);
+ const clickHander=()=>{
+      setTitle('Updated!!!')
+ }
   return (
     <Card className="expense-item">
     <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHander}>change</button>
     </Card>
   );
 }
